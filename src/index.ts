ba41cf3d19d733016fed9b6b14984269eda8b377
Kae Uchoa <kae.uchoa@thoughtworks.com>
2022-09-20 11:43:55 -0300

    feat: move coode back to index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,69 @@
-import App from "./App"
-import ProjectInput from "./ProjectInput"
+// import App from "./App"
+// import { Rendable } from "./interfaces/Rendable"
+// import ProjectInput from "./ProjectInput"
+
+/* ###### INTERFACES #### */
+
+interface Rendable {
+  render(): DocumentFragment | HTMLElement
+}
+
+/* ###### INTERFACES #### */
+
+
+/* ###### CLASSES #### */
+
+class ProjectInput implements Rendable {
+  readonly TEMPLATE_ID = 'project-input'
+  templateElement: HTMLTemplateElement
+  
+  constructor() {
+      this.templateElement = document.getElementById(this.TEMPLATE_ID)! as HTMLTemplateElement
+  }
+
+  render() {
+      return this.templateElement.content
+  }
+}
+
+class SingleProject implements Rendable {
+  readonly TEMPLATE_ID = "single-project"
+  templateElement: HTMLTemplateElement
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      this.TEMPLATE_ID
+    )! as HTMLTemplateElement
+  }
+
+  render() {
+    return this.templateElement.content
+  }
+}
+
+class App {
+  hostElement: HTMLDivElement
+
+  constructor() {
+    this.hostElement = document.getElementById("app")! as HTMLDivElement
+  }
+
+  render(...args: Rendable[]) {
+    for (const arg of args) {
+      this.hostElement.appendChild(arg.render())
+    }
+  }
+}
+
+/* ###### CLASSES #### */
+
+/* ###### BODY #### */
+
+const app = new App()
 
 const projectInput = new ProjectInput()
-const app = new App()
-app.render(projectInput)
+const singleProject = new SingleProject()
+app.render(projectInput, singleProject)
+
+
+/* ###### BODY #### */
--- src/interfaces/Rendable.ts
+++
@@ -1,3 +0,0 @@
-export interface Rendable {
-    render(): DocumentFragment | HTMLElement
-}
