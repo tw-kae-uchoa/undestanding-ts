2a11d1163f69fb1af8254c51d15520bf9bfba87d
Kae Uchoa <kae.uchoa@thoughtworks.com>
2022-10-04 10:53:38 -0300

    refactor: update setup for fields

--- src/index.ts
+++ src/index.ts
@@ -68,48 +68,67 @@ class Validation {
 class ProjectInput implements Rendable {
   readonly STYLE_ID = "user-input"
   formElement: HTMLFormElement
-  titleInput: ValidatableField<HTMLInputElement>
-  descriptionInput: ValidatableField<HTMLTextAreaElement>
-  peopleInput: ValidatableField<HTMLInputElement>
+  titleInput?: ValidatableField<HTMLInputElement>
+  descriptionInput?: ValidatableField<HTMLTextAreaElement>
+  peopleInput?: ValidatableField<HTMLInputElement>
 
-  constructor() {
+  private _getFormElement() {
     const templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement
-    this.formElement = TemplateParser.getHTMLElement(
-      templateElement
-    ) as HTMLFormElement
+    return TemplateParser.getHTMLElement(templateElement) as HTMLFormElement
+  }
+
+  private _getTitleInput() {
+    if (!this.formElement) return
 
     const title = this.formElement.querySelector("#title")! as HTMLInputElement
-    this.titleInput = new ValidatableField(title, (field: HTMLInputElement) =>
+    return new ValidatableField(title, (field: HTMLInputElement) =>
       Validation.required(field)
     )
+  }
+
+  private _getDescriptionInput() {
+    if (!this.formElement) return
 
     const descriptionInput = this.formElement.querySelector(
       "#description"
     )! as HTMLTextAreaElement
 
-    this.descriptionInput = new ValidatableField(
+    return new ValidatableField(
       descriptionInput,
       (field: HTMLTextAreaElement) => Validation.minLength(field, 5)
     )
+  }
 
+  private _getPeopleInput() {
+    if (!this.formElement) return
     const peopleInput = this.formElement.querySelector(
       "#people"
     )! as HTMLInputElement
-    this.peopleInput = new ValidatableField(
-      peopleInput,
-      (field: HTMLInputElement) => Validation.min(field, 2)
+    return new ValidatableField(peopleInput, (field: HTMLInputElement) =>
+      Validation.min(field, 2)
     )
+  }
+
+  constructor() {
+    this.formElement = this._getFormElement()
+    this.titleInput = this._getTitleInput()
+    this.descriptionInput = this._getDescriptionInput()
+    this.peopleInput = this._getPeopleInput()
+
     this.configureSubmitListener()
   }
 
   @bind
   private onSubmitHandler(event: Event) {
     event.preventDefault()
-    if (!this.titleInput.isValid()) alert("title input error")
-    if (!this.descriptionInput.isValid()) alert("description input error")
-    if (!this.peopleInput.isValid()) alert("people input error")
+    if (this.titleInput && !this?.titleInput.isValid())
+      alert("title input error")
+    if (this.descriptionInput && !this.descriptionInput.isValid())
+      alert("description input error")
+    if (this.peopleInput && !this.peopleInput.isValid())
+      alert("people input error")
   }
 
   private configureSubmitListener() {
